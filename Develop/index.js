76401da47b3198b365144431ebd65413aa6360d1
Mason Walker <masonwalker30@gmail.com>
2022-01-09 18:39:36 -0800

    added most of questions, added function for inquirer prompt, added license choices, added TODO sudo code for what to work on to complete project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,122 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function MyReadMe() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your project title?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Write a brief decription of your completed project.",
+      name: "projectDescription",
+    },
+    {
+      type: "input",
+      message: "How can I install you project if needed?",
+      name: "projectInstall",
+    },
+    {
+      type: "input",
+      message: "Choose you appropriate license.",
+      name: "projectLicenseChoice",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+      type: "input",
+      message: "Who worked on this project with you?",
+      name: "projectContributers",
+       },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "githubUsername",
+      },  {
+        type: "input",
+        message: "Please provide a link to your github?",
+        name: "githublink",
+      },  {
+        type: "input",
+        message: "What is your project title?",
+        name: "projectTitle",
+      },  {
+        type: "input",
+        message: "What is your project title?",
+        name: "projectTitle",
+      },  {
+        type: "input",
+        message: "What is your project title?",
+        name: "projectTitle",
+      },
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+
+  ]);
+}
+
+
+
+
+
+
+//TODO: 
+// ask for title DONE
+
+//TODO:
+// ASK USER FOR BRIEF DESCRIPTION OF PROJECT 
+
+//TODO:
+// INSTALLTION: WHAT YOU HAVE TO DO TO USE CREATE 
+// THE READ ME
+
+//TODO:
+// USAGE: WHAT THE READ ME WILL BE USED FOR
+
+//TODO:
+// LICENSE: WATCH LICENSE, SHEILDING, 
+// IT IS A OBJECT WITH A ARRAY 
+{
+    type: "list",
+    name: "license",
+    message: "Chose the appropriate license for this project: ",
+    choices: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open"
+    ]
+},
+
+//TODO:
+//CONTRIBUTING:WHO WORKED ON THE PROJECT
+
+//TODO:
+//TEST: LOOK INTO THIS
+
+//TODO:
+// GIT HUB USER NAME:
+// WHEN GIT HUB USERNAME IS ENETERED, THEN THIS
+// IS ADDED TO THE SECTION OF THE README ENTITLED
+// QUESTIONS WITH A LINK TO MY GITHUB PROFILE
+
+//TODO: 
+// 
+
+//TODO:
+//
