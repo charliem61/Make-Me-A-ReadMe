a093a4734052e7e274d3f164ac6c97158bf1b365
Mason Walker <masonwalker30@gmail.com>
2022-01-23 03:37:04 -0800

    removed old answers and excess wording,added usage prompt/answer, added features prompt/answer, added howtoContribute prompt/answer, GETTING LATE, FINISH SOON.

--- Develop/README.md
+++ Develop/README.md
@@ -1,11 +1,50 @@
 
-      What is your project Title: f
-      Desrciption of Project: adfs
-      How can I install your project if needed? g
-      Please choose your appropriate license? reg
-      Were there any other contributers on this project: er
-      What is your Github Username: er
-      Please provide a link to your Github: er
-      Project Title: f
-      Where can someone reach out to you for additional questions: g
+    # <Weather App>
+    ## Description
+    # <The weather app shows local weather in the user specified area taken from data stored in a applied weather api for that area.>
+    ## Table of Contents (Optional)
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    # <undefined>
+    ## Installation
+    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    # <npm i>
+    ## Usage
+    Provide instructions and examples for use. Include screenshots as needed.
+    To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+        md
+        ![alt text](assets/images/screenshot.png)
+    # <undefined>
+    
+    //TODO:
+      // MAKE USAGE QUESTION ABOVE IN PROMPTS//
+    
+    ## Credits
+    List your collaborators, if any, with links to their GitHub profiles.
+    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+    If you followed tutorials, include links to those here as well.
+    # <Me, Myself, and I>
+    
+    ## License
+    ![gitHubLicense](https://img.shields.io/badge/license-Mozilla-blue.svg)
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    # <Mozilla
+    
+    ## Features
+    If your project has a lot of features, list them here.
+    # <undefined
+
+    //TODO:
+      // MAKE FEATURE QUESTION ABOVE IN PROMPTS//
+
+    ## How to Contribute
+    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+    # <undefined
+
+    //TODO:
+      // MAKE HOW TO CONTRIBUTE QUESTION ABOVE IN PROMPTS//
+    
     
--- Develop/index.js
+++ Develop/index.js
@@ -11,9 +11,7 @@ function MyReadMe() {
         message: "What is your project title?",
         name: "projectTitle",
       },
-      //PROPERTY OR KEY OR PROPERTY NAME- TYPE:
-      // "INPUT"-VALUE
-
+      
       {
         type: "input",
         message: "Write a brief decription of your project:",
@@ -21,8 +19,15 @@ function MyReadMe() {
       },
       {
         type: "input",
-        message: "How can I install your project if needed?",
+        message: 
+        "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
         name: "projectInstall",
+      },
+      {
+        type: "input",
+        message:
+        "Provide instructions and examples for use. Include screenshots as needed.",
+        name: "projectUsage",
       },
       {
         type: "list",
@@ -32,7 +37,8 @@ function MyReadMe() {
       },
       {
         type: "input",
-        message: "Who worked on this project with you?",
+        message: 
+        "Please list your collaborators, if any, with links to their GitHub profiles.",
         name: "projectContributers",
       },
       {
@@ -47,26 +53,31 @@ function MyReadMe() {
       },
       {
         type: "input",
-        message:
-          "Where can someone email you for additional questions?",
+        message: "Where can someone email you for additional questions?",
         name: "userEmail",
       },
+      {
+        type: "input",
+        message: "If your project has many features, list them here:",
+        name: "projectFeatures",
+      },
+      {
+        type:"input",
+        message:
+        "If your created application or package needs outside additions & you would like other developers to contribute to it, please provide a link to request access to contribute.",
+        name: "howToContribute"
+      },
     ])
+
     .then(function (answers) {
-    //   var readmeOutput = `
-    //   What is your project title? ${answers.projectTitle}
-    //   Write a brief desrciption of project: ${answers.projectDescription}
-    //   How can I install your project if needed? ${answers.projectInstall}
-    //   Please choose your appropriate license: ${answers.projectLicenseChoice}
-    //   Who worked on this project with you? ${answers.projectContributers}
-    //   What is your Github username? ${answers.githubUsername}
-    //   Please provide a link to your Github: ${answers.githublink}
-    //   Where can someone email you for additional questions? ${answers.userEmail}
-    // `;
+    
     var readmeOutput = `
+    ## Project Title
     # <${answers.projectTitle}>
+
     ## Description
     # <${answers.projectDescription}>
+
     ## Table of Contents (Optional)
     If your README is long, add a table of contents to make it easy for users to find what they need.
     - [Installation](#installation)
@@ -74,48 +85,28 @@ function MyReadMe() {
     - [Credits](#credits)
     - [License](#license)
     # <${answers.projectTableofContents}>
+
     ## Installation
-    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
     # <${answers.projectInstall}>
+
     ## Usage
-    Provide instructions and examples for use. Include screenshots as needed.
-    To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-        md
-        ![alt text](assets/images/screenshot.png)
     # <${answers.projectUsage}>
     
-    //TODO:
-      // MAKE USAGE QUESTION ABOVE IN PROMPTS//
-    
     ## Credits
-    List your collaborators, if any, with links to their GitHub profiles.
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    If you followed tutorials, include links to those here as well.
     # <${answers.projectContributers}>
     
     ## License
     ![gitHubLicense](https://img.shields.io/badge/license-${answers.projectLicenseChoice}-blue.svg)
     Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    # <${answers.projectLicenseChoice}
+    # <${answers.projectLicenseChoice}>
     
     ## Features
-    If your project has a lot of features, list them here.
-    # <${answers.projectFeatures}
-
-    //TODO:
-      // MAKE FEATURE QUESTION ABOVE IN PROMPTS//
+    # <${answers.projectFeatures}>
 
     ## How to Contribute
-    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-    # <${answers.howToContribute}
-
-    //TODO:
-      // MAKE HOW TO CONTRIBUTE QUESTION ABOVE IN PROMPTS//
+    # <${answers.howToContribute}>
     
     `;
-    
-    
-    
 
 
       // instead of console log, save/write to a file
